664c55ffc2b38946194f9de8d9320cbace1dca05
sanjaymenonar <sanjaymenon61@gmail.com>
2024-05-08 21:18:59 +0530

    first commit new

--- api/config/dbConnection.js
+++ api/config/dbConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require("dotenv").config()
 
 // Create a connection pool with the promise wrapper
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'debian-sys-maint',
-  password: 'alhaR2PRYw7t5LdM',
-  database: 'Products'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 }).promise();
 
 module.exports = pool;
